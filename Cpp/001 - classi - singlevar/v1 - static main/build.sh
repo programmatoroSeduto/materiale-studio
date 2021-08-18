d07063df804e28afd301924d4fdc530da86809e3
programmatoroSeduto <francesc.ganci@gmail.com>
2021-08-18 13:12:00 +0000

    cleaning

--- Cpp/001 - classi - singlevar/v1 - static main/build.sh
+++
@@ -1,6 +0,0 @@
-g++ -c main.cpp
-g++ -c singlevar.cpp
-g++ main.o singlevar.o -o cpp_program
-rm main.o singlevar.o
-./cpp_program
-rm ./cpp_program
--- Cpp/003 - costruttori/build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-g++ threevars.cpp -o threevars 2>buildlog.log
-./threevars
-rm threevars
